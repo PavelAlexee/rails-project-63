05603b06634e0b46e702eb25a25596e2caacae49
PavelAlexee <pavelps41980@gmail.com>
2025-09-11 22:02:15 +0300

    create HexletCode::Tag.build

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,27 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Tag
+
+    ORDINARY_TAGS = %w[br col img link].freeze
+
+    def build(tag_name, **attributes, &block)
+      attrs = build_attributes(attributes)
+      tag = "<#{tag_name}#{attrs}>"
+      end_tag = "</#{tag_name}>"
+
+      if block_given?
+        text = yield
+        
+        "#{tag}#{text}#{end_tag}"
+      else
+        ORDINARY_TAGS.include?(tag_name) ? tag : "#{tag}#{end_tag}"
+      end
+    end
+
+    def build_attributes(attrs)
+      attrs.map { |key, value| " #{key}=\"#{value}\"" }.join
+    end
+  end
 end
