e3db23b20801e8b2ee7bbe8b8a54fbaf10797440
PavelAlexee <pavelps41980@gmail.com>
2025-09-15 22:56:40 +0300

    Quality. add rubocop. fixed ci.yml

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 module HexletCode
-
   autoload :Tag, 'hexlet_code/tag'
   autoload :VERSION, 'hexlet_code/version'
-  
-  class Error < StandardError; end
 
+  class Error < StandardError; end
 end
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -2,17 +2,16 @@
 
 module HexletCode
   class Tag
-
     ORDINARY_TAGS = %w[br col img link].freeze
 
-    def self.build(tag_name, **attributes, &block)
+    def self.build(tag_name, **attributes)
       attrs = build_attributes(attributes)
       tag = "<#{tag_name}#{attrs}>"
       end_tag = "</#{tag_name}>"
 
       if block_given?
         text = yield
-        
+
         "#{tag}#{text}#{end_tag}"
       else
         ORDINARY_TAGS.include?(tag_name) ? tag : "#{tag}#{end_tag}"
