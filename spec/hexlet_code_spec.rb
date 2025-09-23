23e12799ffeb0f8a580eb065bfa9dc69db7d6b7b
PavelAlexee <pavelps41980@gmail.com>
2025-09-23 21:07:39 +0300

    fixed hexlet_code for rubocop #2

--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -8,65 +8,56 @@ User = Struct.new(:name, :job, :gender, keyword_init: true)
 RSpec.describe HexletCode do
   let(:user) { User.new name: 'rob', job: 'hexlet', gender: 'm' }
 
-  describe '.form_for basic forms' do
-    it 'generates a empty form' do
-      generate_tag = HexletCode.form_for user
-      expected_form = '<form action="#" method="post"></form>'
-
-      expect(generate_tag).to eq expected_form
+  describe 'basic form generation' do
+    it 'empty form' do
+      expect(HexletCode.form_for(user)).to eq '<form action="#" method="post"></form>'
     end
 
-    it 'generates a form with class' do
-      generate_tag = HexletCode.form_for user, class: 'hexlet-form'
-      expected_form = '<form action="#" method="post" class="hexlet-form"></form>'
-
-      expect(generate_tag).to eq expected_form
+    it 'form with class' do
+      result = HexletCode.form_for user, class: 'hexlet-form'
+      expect(result).to eq '<form action="#" method="post" class="hexlet-form"></form>'
     end
 
-    it 'generates a form with class and key url' do
-      generate_tag = HexletCode.form_for user, url: '/profile', class: 'hexlet-form'
-      expected_form = '<form action="/profile" method="post" class="hexlet-form"></form>'
-
-      expect(generate_tag).to eq expected_form
+    it 'form with url and class' do
+      result = HexletCode.form_for user, url: '/profile', class: 'hexlet-form'
+      expect(result).to eq '<form action="/profile" method="post" class="hexlet-form"></form>'
     end
   end
 
-  describe '.form_for with different input types' do
-    it 'checks input fields :name and :job' do
-      generate_tag = HexletCode.form_for user do |f|
+  describe 'input fields' do
+    it 'name and job inputs' do
+      result = HexletCode.form_for user do |f|
         f.input :name
         f.input :job, as: :text
       end
-      expected_form = '<form action="#" method="post"><label for="name">Name</label>' \
-                      '<input name="name" type="text" value="rob">' \
-                      '<label for="job">Job</label><textarea name="job" cols="20" rows="40">hexlet' \
-                      '</textarea></form>'
-
-      expect(generate_tag).to eq expected_form
+      expected = '<form action="#" method="post"><label for="name">Name</label>' \
+                 '<input name="name" type="text" value="rob">' \
+                 '<label for="job">Job</label><textarea name="job" cols="20" rows="40">hexlet</textarea></form>'
+      expect(result).to eq expected
     end
 
-    it 'checks input fields with additional attributes' do
-      generate_tag = HexletCode.form_for user, url: '#' do |f|
+    it 'input with custom attributes' do
+      result = HexletCode.form_for user, url: '#' do |f|
         f.input :name, class: 'user-input'
       end
-      expected_form = '<form action="#" method="post"><label for="name">Name</label>' \
-                      '<input name="name" type="text" value="rob" class="user-input"></form>'
-
-      expect(generate_tag).to eq expected_form
-    end
-
-    it 'checks textarea with custom attributes' do
-      generate_tag = HexletCode.form_for user do |f|
-        f.input :job, as: :text, rows: 50, cols: 50
-      end
-      expected_form = '<form action="#" method="post"><label for="job">Job</label>' \
-                      '<textarea name="job" cols="50" rows="50">hexlet</textarea></form>'
-
-      expect(generate_tag).to eq expected_form
+      expected = '<form action="#" method="post"><label for="name">Name</label>' \
+                 '<input name="name" type="text" value="rob" class="user-input"></form>'
+      expect(result).to eq expected
     end
   end
 
-  describe '.form_for error handling' do
+  describe 'textarea fields' do
+    it 'textarea with custom size' do
+      result = HexletCode.form_for user do |f|
+        f.input :job, as: :text, rows: 50, cols: 50
+      end
+      expected = '<form action="#" method="post"><label for="job">Job</label>' \
+                 '<textarea name="job" cols="50" rows="50">hexlet</textarea></form>'
+      expect(result).to eq expected
+    end
+  end
+
+  describe 'error handling' do
     it 'raises error for undefined field' do
       expect do
         HexletCode.form_for user do |f|
@@ -76,34 +67,28 @@ RSpec.describe HexletCode do
     end
   end
 
-  describe '.form_for submit functionality' do
-    it "checks method 'submit'" do
-      generate_tag = HexletCode.form_for user, &:submit
-
-      expected_form = '<form action="#" method="post"><input type="submit" value="Save"></form>'
-
-      expect(generate_tag).to eq expected_form
+  describe 'submit button' do
+    it 'default submit' do
+      result = HexletCode.form_for user, &:submit
+      expect(result).to eq '<form action="#" method="post"><input type="submit" value="Save"></form>'
     end
 
-    it 'check labels with input' do
-      generate_tag = HexletCode.form_for user do |f|
-        f.input :job
-      end
-
-      expected_form = '<form action="#" method="post"><label for="job">Job</label>' \
-                      '<input name="job" type="text" value="hexlet"></form>'
-
-      expect(generate_tag).to eq expected_form
-    end
-
-    it 'check the text for the button' do
-      generate_tag = HexletCode.form_for user do |f|
+    it 'submit with custom text' do
+      result = HexletCode.form_for user do |f|
         f.submit 'Wow'
       end
+      expect(result).to eq '<form action="#" method="post"><input type="submit" value="Wow"></form>'
+    end
+  end
 
-      expected_form = '<form action="#" method="post"><input type="submit" value="Wow"></form>'
-
-      expect(generate_tag).to eq expected_form
+  describe 'labels' do
+    it 'label with input' do
+      result = HexletCode.form_for user do |f|
+        f.input :job
+      end
+      expected = '<form action="#" method="post"><label for="job">Job</label>' \
+                 '<input name="job" type="text" value="hexlet"></form>'
+      expect(result).to eq expected
     end
   end
 end
